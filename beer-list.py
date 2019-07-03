dcb3c956971f2af87f1f5b7ebd15fbe4da5124de
Alex Couture-Beil <alex@mofo.ca>
2019-07-03 12:40:52 -0700

    beer list

---
+++ beer-list.py
@@ -0,0 +1,36 @@
+import sys
+import requests
+import csv
+from bs4 import BeautifulSoup
+beerwriter = csv.writer(sys.stdout)
+
+try:
+    text = open('pouring.html', 'r').read()
+except:
+    asdf
+    r = requests.get('http://farmhousefest.com/2019/pouring/')
+    text = r.text
+    open('pouring.html', 'w').write(text)
+
+soup = BeautifulSoup(text, 'html.parser')
+beer_list = soup.find_all('ul', class_='beer-list')
+assert(len(beer_list) == 1)
+beer_list = beer_list[0]
+breweries = beer_list.find_all('li', recursive=False)
+for brewery in breweries:
+    brewery_name = brewery.find('h3').get_text()
+    pouring = soup.find_all('ul', class_='pouring')
+    for beer in pouring:
+        beer_name = beer.find('h4').get_text()
+        try:
+            beer_abv = beer.find('p', class_='abv').get_text()
+        except:
+            beer_abv = '?'
+        try:
+            beer_desc = beer.find('p', class_='desc').get_text()
+        except:
+            beer_desc = ''
+        beerwriter.writerow((brewery_name, beer_name, beer_abv, beer_desc))
+
+
+
